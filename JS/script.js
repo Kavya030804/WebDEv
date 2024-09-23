a808fe227f1c98ec497483d095f3a3391135a110
kavya agrawal <kavya.agr382004@gmail.com>
2024-09-23 14:06:41 +0530

    new files added

---
+++ JS/script.js
@@ -0,0 +1,28 @@
+// {
+//     var studentId = 3456;
+//     let studentName = "xyz";
+//     const fees = 3456000;
+//     console.log(studentName);
+//     console.log(studentId);
+//     console.log(fees);
+// }
+// console.log(studentName);
+// console.log(studentId);
+// console.log(fees);
+// console.log("hello world");
+
+
+// function sum(){
+//     let sum =55;
+//     if(sum==55){
+//         var fullname = "abcde"
+//     }
+//     console.log(fullname);
+// }
+// sum();
+// console.log(fullname);
+
+var salary = 7346557867;
+let salary = "3543545";
+// var salary = 65453657676548;
+
