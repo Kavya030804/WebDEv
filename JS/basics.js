e9dcaa2a76f1bc3fb6d5999c28a1a567f4f40fcd
kavya agrawal <kavya.agr382004@gmail.com>
2024-09-26 14:37:34 +0530

    files modified

---
+++ JS/basics.js
@@ -0,0 +1,17 @@
+let a=10;
+console.log(typeof(a));
+
+let name = "abcde";
+console.log(typeof(name));
+
+let flag = true;
+console.log(typeof(flag));
+
+let salary;
+console.log(typeof(salary));
+
+let id= null;
+console.log(typeof(id));
+
+let bigNumber = BigInt("56348757789");
+console.log(typeof(bigNumber));
