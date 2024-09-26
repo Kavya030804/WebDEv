80b721a7dc170d6033c66dc9d8b64577aeeb1d06
kavya agrawal <kavya.agr382004@gmail.com>
2024-09-26 14:58:51 +0530

    new files added

---
+++ JS/arr.js
@@ -0,0 +1,9 @@
+let arr=[2,"abcde " , true , null , undefined , []];
+// arr.push("hello");
+//arr.pop();    //remove last element by default
+arr.unshift("hello");
+arr.shift();//remove starting element
+console.log((arr));
+
+// console.log(typeof(arr));
+
