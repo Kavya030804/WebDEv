8cf8ea22531d828e4302e5505c4f690d8d3ee7cc
kavya agrawal <kavya.agr382004@gmail.com>
2024-11-13 15:52:19 +0530

    files added

---
+++ JS/dome.js
@@ -0,0 +1,7 @@
+// let button = document.getElementsByTagName('button');
+// let button = document.getElementsByClassName('btn');
+let button = document.querySelectorAll('.btn');
+let para = document.getElementById('text');
+// para.innerText="hello world";
+para.innerHTML="<h1>hello world</h1>";
+console.log(button);
